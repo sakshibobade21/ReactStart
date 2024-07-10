b01ecae9f6876c346e33eb315230988b85863df5
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-07-10 11:12:59 +0530

    New Simple react code with cdn scripts

---
+++ ReactC2/app.js
@@ -0,0 +1,18 @@
+// <div id="parent">
+//    <div id="child">
+//        <h1>Hey There</h1>
+//        <h2>Sakshi here</h2>
+//    </div>
+// </div>
+
+const element1 = React.createElement("h1", {}, "Hey There");
+const element2 = React.createElement("h2", {}, "Sakshi Here");
+
+const child = React.createElement("div", {id: "child"}, [element1,element2]);
+
+const parent = React.createElement("div", {id: "parent"}, child);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+
