fbb499a3ce4f0ee51d199953cb997221cd13b5bb
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-07-16 10:47:21 +0530

    Updates to the styles

--- project1/src/Components/Navbar.js
+++ project1/src/Components/Navbar.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import '../../styles/Navbar.css';
 
 const Navbar = () => {
   return(
-    <div className="navbar">Navbar here</div>
+    <div className="navbar">
+      <div className="nav-items">
+        <div className="item">Offers</div>
+        <div className="item">Help</div>
+        <div className="item">Sign In</div>
+        <div className="item">Cart</div>
+      </div>
+    </div>
   )
 }
 
